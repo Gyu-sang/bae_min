2667dbd3f2b06828263944eef8f26e3af1d808d5
Gyu-sang <sbluep@hanmail.net>
2019-08-03 18:17:34 +0900

    가게 목록 구현

--- baemin/views.py
+++ baemin/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Shop
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'baemin/index.html', {})
+    return render(request, 'baemin/index.html', {
+        'shop_list': Shop.objects.all(),
+    })
