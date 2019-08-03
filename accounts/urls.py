547072c715222bc1965d9c6df149a04cde1b0d06
Gyu-sang <sbluep@hanmail.net>
2019-08-03 17:42:59 +0900

    로그인/회원가입/로그아웃/프로필 구현 및 연결

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^profile/$', views.profile, name='profile')
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
